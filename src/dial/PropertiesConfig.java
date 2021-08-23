bb81d430598a01d3d33fd64c91f59573c735f8f7
Jessy Snow <jessysnow12345@gmail.com>
2021-08-23 18:23:36 +0800

    Add Some java file about config IO

--- src/dial/PropertiesConfig.java
+++ src/dial/PropertiesConfig.java
@@ -3,15 +3,69 @@ import dial.model.Config;
 import dial.model.User;
 
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 import java.io.FileInputStream;
 
-public class PropertiesConfig extends Config{
-    private String configPath = System.getProperty("user.dir") + "\\resources\\config\\config.properties";
+interface configUser{
+    public void setConfig_UserName(String USER_NAME);
+    public void setConfig_PassWord(String PASS_WORD);
+    public void setConfig_DialRule(String DIAL_RULE);
+    public String getConfig_UserName();
+    public String getConfig_PassWord();
+    public String getConfig_DialRule();
+}
+
+public class PropertiesConfig extends Config implements configUser{
     private Properties configObject;
 
-    PropertiesConfig(User user){
+    public PropertiesConfig(User user){
         super(user);
+        configObject = new Properties();
     }
-    
+
+    public void initUserWithConfig(){
+        user.setUserName(getConfig_UserName());
+        user.setPassWord(getConfig_PassWord());
+        user.setRule(getConfig_DialRule());
+    }
+    public void loadFromDisk(){
+        String configPath = System.getProperty("user.dir") + "\\resources\\config\\config.properties";
+        try {
+            configObject.load(new FileInputStream(configPath));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void writeToDisk(){
+        String configPath = System.getProperty("user.dir") + "\\resources\\config\\config.properties";
+        try {
+            FileOutputStream propsOut = new FileOutputStream(configPath);
+            configObject.store(propsOut, "NULL");
+            propsOut.close();
+            propsOut = null;
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void setConfig_UserName(String USER_NAME){
+        configObject.setProperty("USER_NAME", USER_NAME);
+    }
+    public void setConfig_PassWord(String PASS_WORD){
+        configObject.setProperty("PASS_WORD", PASS_WORD);
+    }
+    public void setConfig_DialRule(String DIAL_RULE){
+        configObject.setProperty("DIAL_RULE", DIAL_RULE);
+    }
+    public String getConfig_UserName(){
+        return configObject.getProperty("USER_NAME");
+    }
+    public String getConfig_PassWord(){
+        return configObject.getProperty("PASS_WORD");
+    }
+    public String getConfig_DialRule(){
+        return configObject.getProperty("DIAL_RULE");
+    }
 }
---
+++ src/dial/test/ConfigTEST.java
@@ -0,0 +1,28 @@
+package dial.test;
+import dial.model.User;
+import dial.PropertiesConfig;
+
+public class ConfigTEST {
+    public static void main(String[] args) {
+        User user = new User();
+        PropertiesConfig props = new PropertiesConfig(user);
+        props.loadFromDisk();
+        props.initUserWithConfig();
+
+        System.out.println(props.getConfig_DialRule());
+        System.out.println(props.getConfig_PassWord());
+
+        System.out.println(user.getUserName());
+        System.out.println(user.getPassWord());
+        System.out.println(user.getRule());
+
+        props.setConfig_DialRule("CMCC");
+        props.setConfig_UserName("Jessy");
+        props.setConfig_PassWord("7878");
+
+        props.writeToDisk();
+        props.initUserWithConfig();
+        System.out.println(user.getRule());
+        System.out.println(user.getPassWord());
+    }
+}
