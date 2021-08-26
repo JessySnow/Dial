1762f21788f9b6b09b07672de44e59110a2e08ab
Jessy Snow <jessysnow12345@gmail.com>
2021-08-26 15:16:51 +0800

    Add Main.java

---
+++ src/dial/Main.java
@@ -0,0 +1,22 @@
+package dial;
+
+import javafx.application.Application;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    private Stage primStage;
+    private Pane userPanel;
+
+    /* Set up for javafx ev */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    /* The main javafx code here */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
