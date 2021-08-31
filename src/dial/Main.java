aa0cbaa76ce0910fe31e2495d3175a69c5873e14
Jessy Snow <jessysnow12345@gmail.com>
2021-08-31 20:15:06 +0800

    Update UI

--- src/dial/Main.java
+++ src/dial/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.util.Objects;
 
@@ -21,9 +22,11 @@ public class Main extends Application{
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/Panel.fxml")));
         Scene scene = new Scene(root);
+
+        /* Edit the stage */
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(scene);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("view/Panel.css")).toExternalForm());
         primaryStage.show();
     }
-
 }
