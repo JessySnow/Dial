69b93692b890e7c036ecae49d47d35590f78f184
Jessy Snow <jessysnow12345@gmail.com>
2021-09-07 16:20:38 +0800

    Update pom.xml

---
+++ src/test/java/modelEx/PropertiesConfigTest.java
@@ -0,0 +1,23 @@
+package modelEx;
+import model.User;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertiesConfigTest {
+    private static User user = new User();
+
+    @org.junit.jupiter.api.Test
+    void testInitUserWithConfig() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void testLoadFromDisk() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void testWriteToDisk() {
+
+    }
+}
